39378fdd439bfb9ad322fce69ecdeb9e61d4bf85
zerolxy <1056180778@qq.com>
2022-07-07 16:25:53 +0800

    'ordercontroller拿到token'

--- src/main/java/com/java1234/controller/OrderController.java
+++ src/main/java/com/java1234/controller/OrderController.java
@@ -5,14 +5,19 @@ package com.java1234.controller;
  * @create 2022-06-09
  */
 import com.java1234.entity.Order;
+import com.java1234.entity.OrderDetail;
 import com.java1234.entity.R;
 import com.java1234.service.IOrderDetailService;
 import com.java1234.service.IOrderService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 /**
  * 测试
  * @author 订单controller控制器
@@ -37,8 +42,21 @@ public class OrderController {
     @RequestMapping("/create")
     public R create(@RequestBody Order order,@RequestHeader(value = "token")String token){
         // 通过token 获取 openid
-        
+        System.out.println("token" + token);
+        // 通过token获取openid
+        System.out.println("token="+token);
+        System.out.println("order="+order);
         // 添加订单到数据库
+        Claims claims = JwtUtils.validateJWT(token).getClaims();
+        if(claims!=null){
+            System.out.println("openid="+claims.getId());
+            order.setUserId(claims.getId());
+        }
+        order.setOrderNo("JAVA"+DateUtil.getCurrentDateStr());
+        order.setCreateDate(new Date());
+
+        OrderDetail[] goods = order.getGoods();
+        orderService.save(order);
 
         // 添加订单详情到数据库
         return R.ok();
